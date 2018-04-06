1de3e5db6110214e11a44ff8929cbb4bd89403e5
Iñigo Biedma <inigobr@cyc.es>
2018-04-06 12:45:26 +0200

    Fix tokeninfo interface, use react refs and embed the report on component mount

--- PowerBIEmbedded/ClientApp/components/PowerBIReport.tsx
+++ PowerBIEmbedded/ClientApp/components/PowerBIReport.tsx
@@ -1,26 +1,33 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
+﻿import * as React from 'react';
+import * as pbi from 'powerbi-client';
 
-export interface TokenInfo {
-    EmbedToken: string,
-    EmbedUrl: string,
-    ReportId: string,
-    mode: string
+export interface TokenInfo extends pbi.IEmbedConfiguration {
+    reportId?: string,
+    mode?: string
 }
 
 export class PowerBIReport extends React.Component<TokenInfo, {}> {
+    private powerbi: pbi.service.Service;
+    private ref: HTMLDivElement;
     constructor(props: TokenInfo) {
         super(props);
+        this.powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
     }
     public render() {
         return (
             <div>
-                <div id='embedContainer'></div>
-                Token: <pre>{this.props.EmbedToken}</pre>
-                Url: <pre>{this.props.EmbedUrl}</pre>
-                ReportId: <pre>{this.props.ReportId}</pre>
-                Mode: <pre>{this.props.mode}</pre>
+                <div className="embedContainer" ref={(div) => { if (div) {this.ref = div; }}}></div>
+                <div>
+                Token: <pre>{this.props.accessToken}</pre>
+                Url: <pre>{this.props.embedUrl}</pre>
+                id: <pre>{this.props.id}</pre>
+                type: <pre>{this.props.type}</pre>
+                mode: <pre>{this.props.mode}</pre>
+                </div>
             </div>
         )
+    }
+    componentDidMount() {
+        this.powerbi.embed(this.ref, this.props);
     }
 }
