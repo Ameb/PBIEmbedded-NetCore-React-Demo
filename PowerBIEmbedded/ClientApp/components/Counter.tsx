a59e8ec4f588a11984d7e787338a78d3e60fe85f
Iñigo Biedma <inigobr@cyc.es>
2018-04-06 10:17:35 +0200

    React v16 changes

--- PowerBIEmbedded/ClientApp/components/Counter.tsx
+++ PowerBIEmbedded/ClientApp/components/Counter.tsx
@@ -6,8 +6,8 @@ interface CounterState {
 }
 
 export class Counter extends React.Component<RouteComponentProps<{}>, CounterState> {
-    constructor() {
-        super();
+    constructor(props: RouteComponentProps<{}>) {
+        super(props);
         this.state = { currentCount: 0 };
     }
 
--- PowerBIEmbedded/ClientApp/components/FetchData.tsx
+++ PowerBIEmbedded/ClientApp/components/FetchData.tsx
@@ -8,8 +8,8 @@ interface FetchDataExampleState {
 }
 
 export class FetchData extends React.Component<RouteComponentProps<{}>, FetchDataExampleState> {
-    constructor() {
-        super();
+    constructor(props: RouteComponentProps<{}>) {
+        super(props);
         this.state = { forecasts: [], loading: true };
 
         fetch('api/SampleData/WeatherForecasts')
