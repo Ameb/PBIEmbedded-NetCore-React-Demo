58715844c994d93440966006894305b3ec705fe3
Iñigo Biedma <inigobr@cyc.es>
2018-04-19 12:56:12 +0200

    Add handler and change list format

--- PowerBIEmbedded/ClientApp/components/PowerBI/ReportPicker.tsx
+++ PowerBIEmbedded/ClientApp/components/PowerBI/ReportPicker.tsx
@@ -19,6 +19,7 @@ export class ReportPicker extends React.Component<RouteComponentProps<{}>, Repor
         super(props);
         this.state = {reports: undefined, loading: true};
         this.updateData();
+        this.handleEdit = this.handleEdit.bind(this);
     }
     private updateData() {
         let url = 'api/PowerBI/GetReportList';
@@ -35,18 +36,19 @@ export class ReportPicker extends React.Component<RouteComponentProps<{}>, Repor
             : this.renderContainer();
         return contents;
     }
+    private handleEdit(reportId: String) {
+        console.log(reportId);
+    }
     private renderContainer() {
         if (!this.state.reports) return;
-        function ReportLine(report: Report) {
-            return (
-                <li className="list-group-item" key={report.id}>{report.name}</li>
+        const ReportLine = (report: Report, onclick: Function) => (
+                <a className="list-group-item" key={report.id}>{report.name} <a onClick={() => onclick(report.id)} className="pull-right"><span className="glyphicon glyphicon-pencil"></span></a></a>
             )
-        }
         return (
-            <div className="col-md-6">
-                <ul className="list-group">
-                    {this.state.reports.map(report => <ReportLine {...report} />)}
-                </ul>
+            <div className=" col-md-6">
+                <div className="list-group">
+                    {this.state.reports.map(report => ReportLine(report, this.handleEdit))}
+                </div>
             </div>
         )
     }
